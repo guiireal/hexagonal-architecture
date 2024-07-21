66f6cda5e92a94aa2f4841c87aaae5dd1ce5a3c3
guiireal <guii_real@hotmail.com>
2024-07-20 21:30:38 -0300

    wip

---
+++ scripts/db.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
